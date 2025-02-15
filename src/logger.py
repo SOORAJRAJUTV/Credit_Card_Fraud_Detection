90241c978f3338ae5208075d06012b32507eca03
sooraj <soorajraju9485@gmail.com>
2025-02-15 13:18:33 +0530

    Data Ingestion done

--- src/logger.py
+++ src/logger.py
@@ -16,6 +16,6 @@ logging.basicConfig(
     level=logging.INFO,
 )
 
-if __name__ == "__main__":
-     logging.info("Logging has started")
+# if __name__ == "__main__":
+#      logging.info("Logging has started")
 
